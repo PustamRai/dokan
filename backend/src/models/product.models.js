5e2eb50f6db2131cbef475bef3d629a731804b04
Pustam Rai <github.pustam@gmail.com>
2025-03-18 09:39:07 +0545

    add product and user models

---
+++ backend/src/models/product.models.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: Array,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    subCategory: {
+      type: String,
+      required: true,
+    },
+    sizes: {
+      type: Array,
+      required: true,
+    },
+    bestseller: {
+      type: Boolean,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const ProductModel = mongoose.model("Product", productSchema);
---
+++ backend/src/models/user.models.js
@@ -0,0 +1,28 @@
+import mongoose, { mongo, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    cartData: {
+      type: Object,
+      default: {},
+    },
+  },
+  {
+    minimize: false,
+  }
+);
+
+export const UserModel = mongoose.model("User", userSchema)
