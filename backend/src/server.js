9d04b518d3d8d8d478e0541702a3b549a5f4e6d6
Pustam Rai <github.pustam@gmail.com>
2025-03-18 08:56:03 +0545

    add connect to mongodb

---
+++ backend/src/server.js
@@ -0,0 +1,28 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8002
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`server is listening at PORT ${PORT}`)
+    })
+})
+.catch((error) => {
+    console.error("connection to mongodDB failed...", error.message)
+    process.exit(1) // process code 1 means exit with failure, 0 means success
+})
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// api endpoints
+app.get("/", (req, res) => {
+    res.send("welcome to dokan")
+})
