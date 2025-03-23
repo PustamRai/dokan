b05f0a7b4da3877998fee620d6556eab36d8885d
Pustam Rai <github.pustam@gmail.com>
2025-03-23 23:48:41 +0545

    add token expiry

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -6,7 +6,8 @@ import jwt from "jsonwebtoken";
 const createToken = (id) => {
   return jwt.sign(
     { id }, 
-    process.env.JWT_SECRET
+    process.env.JWT_SECRET,
+    { expiresIn: "3h" }
   );
 };
 
