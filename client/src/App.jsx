6e50e2ca647dfb37625b651770a56eb482c6b996
Pustam Rai <github.pustam@gmail.com>
2025-03-25 09:52:38 +0545

    add route for navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product"
+import Cart from "./pages/Cart"
+import Login from "./pages/Login"
+import PlaceOrder from "./pages/PlaceOrder"
+import Orders from "./pages/PlaceOrder"
+import PageNotFound from "./pages/PageNotFound"
 
 
 function App() {
-
   return (
-    <div>
-      <h2 className='bg-green-500'>welcome to our dokan</h2>
+    <div className="bg-gray-200 px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Layout /> }>
+            <Route index element={ <Home /> } />
+            <Route path="*" element={ <PageNotFound /> } />
+            <Route path="/about" element={ <About /> } />
+            <Route path="/collection" element={ <Collection /> } />
+            <Route path="/contact" element={ <Contact /> } />
+            <Route path="/product" element={ <Product /> } />
+            <Route path="/cart" element={ <Cart /> } />
+            <Route path="/login" element={ <Login /> } />
+            <Route path="/place-order" element={ <PlaceOrder /> } />
+            <Route path="/orders" element={ <Orders /> } />
+          </Route>
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
