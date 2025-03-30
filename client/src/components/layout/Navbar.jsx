189e4b68dabe7454ca0f625c98fda8923c02d251
Pustam Rai <github.pustam@gmail.com>
2025-03-30 21:22:53 +0545

    improve navbar login

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -5,32 +5,30 @@ import { useProductContext } from "../../context/productContext";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { showSearch, setShowSearch } = useProductContext()
-  const navigate = useNavigate()
+  const { showSearch, setShowSearch } = useProductContext();
+  const navigate = useNavigate();
 
-  const { setToken } = useProductContext()
+  const { token, setToken } = useProductContext();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   const logout = () => {
-    localStorage.removeItem('token')
-    setToken('')
-    navigate('/')
-  }
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+  };
 
   return (
     <nav className="w-full py-4 px-3 md:px-32 flex items-center justify-between bg-gray-50 shadow-sm">
       {/* Logo */}
-      <NavLink
-      to="/"
-      >
-      <div className="flex items-center">
-        <h1 className="text-xl font-bold">
-          DOKAN<span className="text-gray-400">.</span>
-        </h1>
-      </div>
+      <NavLink to="/">
+        <div className="flex items-center">
+          <h1 className="text-xl font-bold">
+            DOKAN<span className="text-gray-400">.</span>
+          </h1>
+        </div>
       </NavLink>
 
       {/* Desktop Navigation */}
@@ -66,9 +64,9 @@ const Navbar = () => {
 
       {/* Icons */}
       <div className="flex items-center space-x-4">
-        <button 
-        className="p-1 hover:text-gray-500 transition-colors hover:cursor-pointer"
-        onClick={() => setShowSearch(!showSearch)}
+        <button
+          className="p-1 hover:text-gray-500 transition-colors hover:cursor-pointer"
+          onClick={() => setShowSearch(!showSearch)}
         >
           <BiSearch size={20} />
         </button>
@@ -80,35 +78,44 @@ const Navbar = () => {
           </span>
         </button>
 
-        {/* <button className="p-1 hover:text-gray-500 transition-colors hover:cursor-pointer">
-          <BiUser size={20} />
-        </button> */}
-
         <div className="relative group/dropdown">
-        <NavLink to='/login'>
-        <button className="p-1 hover:text-gray-500 transition-colors hover:cursor-pointer">
-            <BiUser size={20} />
-          </button>
-        </NavLink>
-        
+          {token ? (
+            <button className="p-1 hover:text-gray-500 transition-colors hover:cursor-pointer">
+              <BiUser size={20} />
+            </button>
+          ) : (
+            <NavLink to="/login">
+              <button
+                type="submit"
+                className="w-full md:w-auto md:px-12 px-2 py-2 bg-black text-white text-sm font-medium rounded-xs hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 mx-auto block cursor-pointer font-mono"
+              >
+                logIn
+              </button>
+            </NavLink>
+          )}
 
           {/* Dropdown Menu */}
-          <div className="absolute top-full right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-50opacity-0 invisible group-hover/dropdown:visible group-hover/dropdown:opacity-100 hover:visible hover:opacity-100 transition-all duration-300 ease-in-out">
-            <div className="py-1">
-              <button className="w-full text-left px-4 py-2 hover:bg-gray-100 transition-colors cursor-pointer">
-                My Profile 
-              </button>
-              <button className="w-full text-left px-4 py-2 hover:bg-gray-100 transition-colors cursor-pointer">
-                Orders
-              </button>
-              <button 
-              className="w-full text-left px-4 py-2 hover:bg-gray-100 transition-colors cursor-pointer"
-              onClick={logout}
-              >
-                Logout
-              </button>
+          {token && (
+            <div className="absolute top-full right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-50opacity-0 invisible group-hover/dropdown:visible group-hover/dropdown:opacity-100 hover:visible hover:opacity-100 transition-all duration-300 ease-in-out">
+              <div className="py-1">
+                <button className="w-full text-left px-4 py-2 hover:bg-gray-100 transition-colors cursor-pointer">
+                  My Profile
+                </button>
+                <button 
+                className="w-full text-left px-4 py-2 hover:bg-gray-100 transition-colors cursor-pointer"
+                onClick={() => navigate('/orders')}
+                >
+                  Orders
+                </button>
+                <button
+                  className="w-full text-left px-4 py-2 hover:bg-gray-100 transition-colors cursor-pointer"
+                  onClick={logout}
+                >
+                  Logout
+                </button>
+              </div>
             </div>
-          </div>
+          )}
         </div>
 
         {/* Mobile Menu Button */}
@@ -124,10 +131,10 @@ const Navbar = () => {
       {isMenuOpen && (
         <div className="md:hidden absolute top-16 left-0 right-0 bg-gray-200 shadow-md py-4 px-6 z-50">
           <div className="flex flex-col space-y-4">
-            <NavLink 
-            to="/" 
-            className="flex flex-col items-center" 
-            onClick={() => setIsMenuOpen(false)}
+            <NavLink
+              to="/"
+              className="flex flex-col items-center"
+              onClick={() => setIsMenuOpen(false)}
             >
               <p className="text-sm uppercase hover:text-gray-500 transition-colors">
                 Home
@@ -135,10 +142,10 @@ const Navbar = () => {
               <hr className="w-5 border-none h-[1.5px] bg-gray-700 hidden" />
             </NavLink>
 
-            <NavLink 
-            to="/about" 
-            className="flex flex-col items-center" 
-            onClick={() => setIsMenuOpen(false)}
+            <NavLink
+              to="/about"
+              className="flex flex-col items-center"
+              onClick={() => setIsMenuOpen(false)}
             >
               <p className="text-sm uppercase hover:text-gray-500 transition-colors">
                 About
@@ -146,10 +153,10 @@ const Navbar = () => {
               <hr className="w-5 border-none h-[1.5px] bg-gray-700 hidden" />
             </NavLink>
 
-            <NavLink 
-            to="/collection" 
-            className="flex flex-col items-center" 
-            onClick={() => setIsMenuOpen(false)}
+            <NavLink
+              to="/collection"
+              className="flex flex-col items-center"
+              onClick={() => setIsMenuOpen(false)}
             >
               <p className="text-sm uppercase hover:text-gray-500 transition-colors">
                 Collection
@@ -157,10 +164,10 @@ const Navbar = () => {
               <hr className="w-5 border-none h-[1.5px] bg-gray-700 hidden" />
             </NavLink>
 
-            <NavLink 
-            to="/contact" 
-            className="flex flex-col items-center"
-            onClick={() => setIsMenuOpen(false)}
+            <NavLink
+              to="/contact"
+              className="flex flex-col items-center"
+              onClick={() => setIsMenuOpen(false)}
             >
               <p className="text-sm uppercase hover:text-gray-500 transition-colors">
                 Contact
