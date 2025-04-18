d68579f721dfcf0c6af191855fa96c0de1045bca
Pustam Rai <github.pustam@gmail.com>
2025-04-18 20:37:28 +0545

    implement Footer component with copyright and links

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="bg-gray-800 text-white py-6 mt-10">
+      <div className="container mx-auto px-4 flex flex-col sm:flex-row justify-between items-center text-sm">
+        <p>&copy; {new Date().getFullYear()} Dokan. All rights reserved.</p>
+        <div className="flex gap-4 mt-2 sm:mt-0">
+          <a href="#" className="hover:underline">Privacy Policy</a>
+          <a href="#" className="hover:underline">Terms of Service</a>
+          <a href="#" className="hover:underline">Contact</a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
