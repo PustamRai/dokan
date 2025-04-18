858a4cc24130e3ee16f0d46b7eba2f3a04d2e0e4
Pustam Rai <github.pustam@gmail.com>
2025-04-18 20:41:27 +0545

    change background color

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-6 mt-10">
+    <footer className="bg-gray-50 py-6 mt-10">
       <div className="container mx-auto px-4 flex flex-col sm:flex-row justify-between items-center text-sm">
         <p>&copy; {new Date().getFullYear()} Dokan. All rights reserved.</p>
         <div className="flex gap-4 mt-2 sm:mt-0">
