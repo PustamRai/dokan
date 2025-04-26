d8acc8e0f4127b1b7241eb5a307311945f32d5bd
Pustam Rai <github.pustam@gmail.com>
2025-04-27 01:04:56 +0545

    improve styling for links

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -3,9 +3,22 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-gray-50 py-6 mt-10 max-w-[1400px] mx-auto">
-      <div className="container mx-auto px-4 flex flex-col sm:flex-row justify-between items-center text-sm">
+      <div className="container mx-auto px-4 flex flex-col sm:flex-row md:px-32 justify-between items-center text-sm">
         <p>&copy; {new Date().getFullYear()} Dokan. All rights reserved.</p>
-        <p className="text-[10px]">
+        
+        <div className="flex gap-4 mt-2 sm:mt-0">
+          <a href="#" className="hover:underline">
+            Privacy Policy
+          </a>
+          <a href="#" className="hover:underline">
+            Terms of Service
+          </a>
+          <a href="#" className="hover:underline">
+            Contact
+          </a>
+        </div>
+
+        <p className="text-[7px]">
           Designed & Developed by:{" "}
           <a
             href="https://www.linkedin.com/in/pustamrai"
@@ -18,17 +31,6 @@ function Footer() {
             </b>{" "}
           </a>
         </p>
-        <div className="flex gap-4 mt-2 sm:mt-0">
-          <a href="#" className="hover:underline">
-            Privacy Policy
-          </a>
-          <a href="#" className="hover:underline">
-            Terms of Service
-          </a>
-          <a href="#" className="hover:underline">
-            Contact
-          </a>
-        </div>
       </div>
     </footer>
   );
